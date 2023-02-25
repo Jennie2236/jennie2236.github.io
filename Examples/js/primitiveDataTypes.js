4d4771363c789dff50b12d5d51e74826e4e878ee
Jennie2236 <Jennie2236@gmail.com>
2023-02-24 20:48:47 -0500

    more

--- Examples/js/primitiveDataTypes.js
+++ Examples/js/primitiveDataTypes.js
@@ -20,3 +20,47 @@ console.log (10/2);
 // String operators
 // concatenation (=) (putting 2strings together)
 console.log("hello" + "world" + "!!!!");
+
+//data type: Boolean (true/false)
+console.log(true);
+console.log(false);
+
+//Boolean operators
+// equivalance (==) (is equal  to)
+console.log(10 ==10);//true
+console.log(10 == 5);//false
+
+// not equialent (!=)  (is not equal to)
+console.log(5 != 15);//true
+console.log(5 != 5);//false
+
+//less than (<)
+console.log(5 < 10);//true
+console.log(10 < 5);//false
+
+//greater than (>)
+console.log(5 > 10);//false
+console.log(10 > 5);//true
+
+// greater than or equal  to (>=)
+//less than or equal to (<=)
+console.log(5 >= 10);//false
+console.log(5 >= 5 );//true
+
+//Boolean data  types allow us  to
+//  write branching code paths
+if (true){
+    console.log("this is a true statement");
+} else {
+    console.log("this statement is false")
+}
+
+//Boolean data  types allow us  to
+//  write branching code paths 
+let a = 5;
+let b = 10;
+if (a > b) {
+    console.log("this is a true statement");
+} else {
+    console.log("this statement is false")
+}
