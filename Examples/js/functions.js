8e394af938ebca2ae9f74c4f89219fdaa4a6091b
Jennie2236 <Jennie2236@gmail.com>
2023-02-28 20:37:10 -0500

    Update functions.js

--- Examples/js/functions.js
+++ Examples/js/functions.js
@@ -20,6 +20,24 @@ greetUser("Jen");
 // function with inout paramete
 // that modify  the page
 function  greetUserOnPage(username) {
-    let greetingDisplay = document.getElementById("greeting display");
-    greetingDisplay.innferHTML= greeting + ", " + username;
+    let greetingDisplay = document.getElementById("greetingDisplay");
+    greetingDisplay.innerHTML= greeting + ", " + username;
 }
+
+greetUserOnPage("Jen");
+
+//funcction that outputs (or "returns")
+function getUserGreeting(username) {
+    return greeting + ", " + username + "! How can I help you?";
+}
+
+console.log(getUserGreeting("Jen"));
+
+//  function using another function
+//that modifiesthe page
+function greetUserOnPageV2(username){
+    let greetingDisplay = document.getElementById("greetingDisplay");
+    greetingDisplay.innerHTML = getUserGreeting(username);
+}
+
+greetUserOnPageV2("Jennifer Figueroa")
