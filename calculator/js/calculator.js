36e0f85b42d2278811cbf775351ad5478808a68c
Jennie2236 <Jennie2236@gmail.com>
2023-03-01 20:28:26 -0500

    update

--- calculator/js/calculator.js
+++ calculator/js/calculator.js
@@ -15,3 +15,12 @@ function displayThreeTimesFour() {
 function displaytwelveTimesTwelve() {
     result.innerHTML = 12 * 12;
 }
+
+function displayMultiply() {
+    let input1 = document.getElementById("input1");
+    let input2 = document.getElementById("input2");
+    result.innerHTML = input1.value * input2.value;
+}
+
+displayMultiply();
+
