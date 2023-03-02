8704b4513e666704b47e96be0d173e517eb3c8bc
Jennie2236 <Jennie2236@gmail.com>
2023-03-01 19:15:01 -0500

    calculator

---
+++ calculator/js/calculator.js
@@ -0,0 +1,17 @@
+let result = document.getElementById("result");
+
+function displayTwoPlusTwo() {
+    let number1 = 2;
+    let number2 = 2;
+    result.innerHTML = number1 + number2;
+}
+
+displayTwoPlusTwo();
+
+function displayThreeTimesFour() {
+    result.innerHTML = 3 * 4;
+}
+
+function displaytwelveTimesTwelve() {
+    result.innerHTML = 12 * 12;
+}
