67db77edd4c0c5807e65f78b932c84c7eced0a23
Jennie2236 <Jennie2236@gmail.com>
2023-03-02 19:09:18 -0500

    lightDark

---
+++ Examples/js/lightDark.js
@@ -0,0 +1,40 @@
+let mode = "light";
+
+let storedMode = localStorge.getItem("mode");
+if (storedMode != null) {
+    mode =  storedMode;
+}
+
+const body = document.getElementById("body");
+const header = document.getElementById("header");
+const button = document.getElementById("modeToggle");
+
+body.style.margin = "0px";
+header.style.padding = "24px";
+
+function displayMode() {
+        //conditional Example
+    if (mode == "light") {
+        header.style.backgoundColor = "black";
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+    } else if (mode == "dark") {
+        header.style.backgoundColor = "white";
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+    }
+}
+
+displayMode();
+
+function toggleMode() {
+    if (mode == "light") {
+        mode = "dark";
+    } else if (mode == "dark") {
+        mode = "light";
+    }
+    localStorage.setItem("mode", mode);
+    displayMode();
+}
+
+button.onclick = toggleMode;
