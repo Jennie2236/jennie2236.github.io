550dd14d9577202ebf5730c0ea35708b524585bb
Jennie2236 <Jennie2236@gmail.com>
2023-03-03 21:43:23 -0500

    Update lightbox.js

--- lightBox/js/lightbox.js
+++ lightBox/js/lightbox.js
@@ -27,8 +27,8 @@ function openLightbox() {
 // let wildlifeImage = document.getElementById("wildlifeImage");
 // wildlifeImage.onclick = openLightbox;
 
-let galleryImages = document.getElementsByClassName('galleryImage');
-for (let i = 0; i < galleryImages.length; i++) {
+let galleryImage = document.getElementsByClassName('galleryImage');
+for (let i = 0; i < galleryImage.length; i++) {
     let galleryImage = galleryImages[i];
     galleryImage.onclick = openLightbox;
 }   
