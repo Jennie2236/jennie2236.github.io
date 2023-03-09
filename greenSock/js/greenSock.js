93e167a987b2e54f067868766591681422083c5c
Jennie2236 <Jennie2236@gmail.com>
2023-03-08 20:52:29 -0500

    Update greenSock.js

--- greenSock/js/greenSock.js
+++ greenSock/js/greenSock.js
@@ -1,4 +1,15 @@
 gsap.from(".chucked", {
     duration: 5, 
     rotation: 720,
-})
+    x: -500,
+    y: -500,
+    ease: "elastic",
+    stagger: 0.5,
+});
+
+gsap.from(".healine", {
+    duration: 2.5,
+    opacity: 0,
+    y: 300,
+    stagger: 0.3,
+});
